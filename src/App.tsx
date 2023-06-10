15fd103b2bad2103869212ba6fe608023c0595eb
deepakbhattmits <deepakbhattmits@gmail.com>
2023-06-10 13:23:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,169 +1,152 @@
+import { useState, FC, Fragment, useContext } from 'react'
 import {
-  useState,
-  FC,
-  // ButtonHTMLAttributes,
-  ChangeEvent,
-  ChangeEventHandler,
-  // EventHandler,
-  MouseEventHandler,
-  useEffect,
-} from 'react'
-import './App.css'
+  Form,
+  Input,
+  message,
+  Button,
+  Space,
+  List,
+  Typography,
+  Divider,
+} from 'antd'
+import { CloseCircleTwoTone, CheckCircleTwoTone } from '@ant-design/icons'
+import TodosProvider, { TodosContext } from './TodosContext'
+
+import './App.less'
+import ButtonComp from './components/ButtonComp'
+// import InputComp from './components/InputComp'
 import Layout from './components/Layout'
-
-var CustomMap: { [key: string]: any } = {}
-const App: FC = (): JSX.Element => {
-  const [todo, setTodo] = useState('')
-  const [todoList, setTodoList] = useState<string[]>([])
-  const [editTodoIdx, setEditTodoIdx] = useState<any>()
-  const [doneMap, setDoneMap] = useState<typeof CustomMap>({})
-
-  useEffect(() => {
-    const eListStr: string | null = localStorage.getItem('todoList') ?? ''
-    const existingList: string[] =
-      eListStr?.length > 0 ? JSON.parse(eListStr) : []
-    setTodoList(existingList)
-    let doneMap: typeof CustomMap
-    if (localStorage.getItem('doneMap')) {
-      doneMap = JSON.parse(localStorage.getItem('doneMap') ?? '')
-    } else {
-      doneMap = {}
-    }
-    setDoneMap(doneMap)
-  }, [])
-
-  const getExistingList = (): string[] => {
-    const eListStr: string | null = localStorage.getItem('todoList') ?? ''
-    const existingList: string[] =
-      eListStr?.length > 0 ? JSON.parse(eListStr) : []
-    return existingList
-  }
-
-  const saveTodoList = (toSave: string[]) => {
-    localStorage.setItem('todoList', JSON.stringify(toSave))
-    setTodoList(toSave)
-  }
-
-  const enterTodo: MouseEventHandler<Element> = () => {
-    const existingList = getExistingList()
-    if (editTodoIdx !== '') {
-      existingList[editTodoIdx] = todo
-    } else if (!!todo?.length) {
-      existingList.push(todo)
-    }
-    localStorage.setItem('todoList', JSON.stringify(existingList))
-    setTodoList(existingList)
+const Todos: FC = (): JSX.Element => {
+  const [form] = Form.useForm()
+  const [todo, setTodo] = useState<string>('')
+  const { todos, addTodo, removeTodo, markCompleted } = useContext(TodosContext)
+  const handleSubmit = () => {
+    addTodo(todo)
     setTodo('')
-    setEditTodoIdx('')
+    form.setFieldsValue({
+      Todo: '',
+    })
+    message.success('Submit success!')
+  }
+  const onFinishFailed = () => {
+    message.error('Submit failed!')
+  }
+  const onFill = () => {
+    form.setFieldsValue({
+      Todo: 'Demo Todo',
+    })
+    setTodo('Demo Todo')
   }
 
-  const editTodo: MouseEventHandler<Element> = (
-    btn: React.MouseEvent<HTMLElement>,
-  ) => {
-    let temp = btn.target as HTMLTextAreaElement
-    const existingList = getExistingList()
-    const nBeingEdited = Number(temp.value)
-    const todo = existingList[Number(temp.value)]
-    setTodo(todo)
-    setEditTodoIdx(`${nBeingEdited}`)
-  }
-  const handleChange: ChangeEventHandler<Element> = (event: ChangeEvent) => {
-    var elem = event.target as HTMLTextAreaElement
+  const Header: FC = (): JSX.Element => (
+    <>
+      <span>Total Todo{todos?.length > 1 ? 's' : ''}: </span>
+      {todos?.length > 0 ? (
+        <>
+          <span>
+            {todos?.length} todo{todos?.length > 1 ? 's, ' : ', '}
+          </span>
+          <span>
+            {todos?.filter(({ completed }) => completed)?.length} Completed,
+          </span>
+          <span>
+            {todos?.filter(({ completed }) => !completed)?.length} Incompleted
+          </span>
+        </>
+      ) : null}
+    </>
+  )
 
-    setTodo(elem.value)
-  }
-  const saveDoneTodo = (idxNoStr: string) => {
-    const doneMap: typeof CustomMap = JSON.parse(
-      localStorage.getItem('doneMap') ?? '{}',
-    )
-    const idx = Number(idxNoStr)
-    doneMap[idx] = !doneMap[idx]
-    setDoneMap(doneMap)
-    localStorage.setItem('doneMap', JSON.stringify(doneMap))
-  }
-  const markDone: MouseEventHandler<Element> = (
-    btn: React.MouseEvent<HTMLElement>,
-  ) => {
-    const idxNoStr = (btn.target as HTMLTextAreaElement).value
-    saveDoneTodo(idxNoStr)
-  }
-  const markDoneChange: ChangeEventHandler<Element> = (event: ChangeEvent) => {
-    var elem = event.target as HTMLTextAreaElement
-    saveDoneTodo(elem.value)
-  }
-  const deleteTodo: MouseEventHandler<Element> = (
-    btn: React.MouseEvent<HTMLElement>,
-  ) => {
-    const idxNoStr = (btn.target as HTMLTextAreaElement).value
-    const idx = Number(idxNoStr)
-    const existingList = getExistingList()
-    existingList.splice(idx, 1)
-    saveTodoList(existingList)
-    setTodo('')
-  }
   return (
-    <div className="App">
-      <Layout headerContent="My todo list" />
+    <div className="TodoArea">
       <div>
-        <div>
-          <div className="TodoArea">
-            <div>
-              <input
-                type="text"
-                placeholder="enter todo"
-                onChange={handleChange}
+        <Divider orientation="left">Todo Form</Divider>
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={handleSubmit}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <div style={{ overflow: 'hidden' }}>
+            <Form.Item
+              name="Todo"
+              label="Enter todo"
+              rules={[{ required: true }, { type: 'string', min: 6 }]}
+            >
+              <Input
+                placeholder="Todo"
                 value={todo}
+                allowClear
+                onChange={(e) => {
+                  setTodo(e?.target?.value)
+                }}
               />
-              <button onClick={enterTodo}> Save</button>
-            </div>
-            <ul>
-              {todoList.map((t: string, idx: number) => (
-                <div className="Todo" key={idx}>
-                  {doneMap[idx] ? (
-                    <input
-                      type="checkbox"
-                      checked
-                      value={'' + idx}
-                      onChange={markDoneChange}
-                    />
-                  ) : (
-                    <input
-                      type="checkbox"
-                      value={'' + idx}
-                      onChange={markDoneChange}
-                    />
-                  )}
-                  <li> {t} </li>
-                  <p style={{ marginLeft: 10 }}>
-                    <button value={'' + idx} onClick={editTodo}>
-                      {' '}
-                      Edit
-                    </button>
-                    <button
-                      style={{ marginLeft: 10 }}
-                      value={'' + idx}
-                      onClick={deleteTodo}
-                    >
-                      {' '}
-                      Delete
-                    </button>
-                    <button
-                      style={{ marginLeft: 10 }}
-                      value={'' + idx}
-                      onClick={markDone}
-                    >
-                      {' '}
-                      Done
-                    </button>
-                  </p>
-                </div>
-              ))}
-            </ul>
+            </Form.Item>
           </div>
-        </div>
+          <Form.Item>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+              <Button htmlType="button" onClick={onFill}>
+                Fill
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+      <div>
+        <ul className="todo-list">
+          <Fragment>
+            <Divider orientation="left">Todo List</Divider>
+            <List
+              header={<Header />}
+              // footer={<div>Footer</div>}
+              bordered
+              dataSource={todos}
+              renderItem={({ id, title, completed }) => (
+                <List.Item>
+                  <div className="left">
+                    {completed ? (
+                      <CheckCircleTwoTone twoToneColor="green" />
+                    ) : (
+                      <CloseCircleTwoTone twoToneColor="#eb2f96" />
+                    )}
+                    <Typography.Text
+                      style={{ marginLeft: '10px' }}
+                      delete={completed}
+                    >
+                      {title}
+                    </Typography.Text>
+                  </div>
+                  <div className="right">
+                    <ButtonComp
+                      style={{ marginRight: '10px' }}
+                      btnText={completed ? 'Completed' : 'Incomplete'}
+                      type={completed ? 'default' : 'primary'}
+                      onClick={() => markCompleted(id)}
+                    />
+                    <ButtonComp
+                      btnText="Delete"
+                      type="primary"
+                      onClick={() => removeTodo(id)}
+                    />
+                  </div>
+                </List.Item>
+              )}
+            />
+          </Fragment>
+          {/* ))} */}
+        </ul>
       </div>
     </div>
   )
 }
-
+const App: FC = (): JSX.Element => (
+  <TodosProvider>
+    <Layout headerContent="My todo list" />
+    <Todos />
+  </TodosProvider>
+)
 export default App

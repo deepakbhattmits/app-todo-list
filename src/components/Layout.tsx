86a5625965069fddbfdd08fb45e51bf5e1d60a82
deepakbhattmits <deepakbhattmits@gmail.com>
2023-06-18 10:36:46 +0530

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react'
+import { Space } from 'antd'
 import styles from './Layout.module.less'
+import ThemeChanger from './ThemeChanger'
 interface IProps {
   headerContent: string
 }
 const Layout: FC<IProps> = ({ headerContent }): JSX.Element => {
-  return <header className={styles.header}>{headerContent}</header>
+  return (
+    <header className={styles.header}>
+      <Space.Compact>
+        {headerContent}
+        <ThemeChanger />
+      </Space.Compact>
+    </header>
+  )
 }
 export default Layout

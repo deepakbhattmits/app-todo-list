a43d950911f78bccac20d56d8d529bfd3cb8879b
deepakbhattmits <deepakbhattmits@gmail.com>
2023-07-25 13:21:12 +0530

    Update types.ts

--- src/TodosContext/types.ts
+++ src/TodosContext/types.ts
@@ -1,11 +1,13 @@
-export type TodosContextState = {
-  // todos: string[];
-  todos: {id: string,title: string;completed:boolean}[];
-  addTodo: (name: string) => void;
-  removeTodo: (id: string) => void;
-  markCompleted: (id: string) => void;
-};
-
 export interface Itodos{
   id: string,title: string; completed: boolean;
 }
+export type TodosContextState={
+  todos: Itodos[];
+  editTodoId: string;
+  addTodo: (name: string) => void;
+  removeTodo: (id: string) => void;
+  markCompleted: (id: string) => void;
+  editTodo: (id: string) => void;
+  updateTodoFunc: (id?: string,title?: string, completed?: boolean) => void;
+};
+

2b1d15da11cf026ab5f0a5b472d9b639d0fd14ed
deepakbhattmits <deepakbhattmits@gmail.com>
2023-07-25 13:21:06 +0530

    Update TodosContext.tsx

--- src/TodosContext/TodosContext.tsx
+++ src/TodosContext/TodosContext.tsx
@@ -1,70 +1,88 @@
-import { createContext, useState, useEffect, FC, ReactNode } from 'react'
-import { TodosContextState, Itodos } from './types'
+import { createContext, useState, useEffect, FC, ReactNode } from "react";
+import { TodosContextState, Itodos } from "./types";
 
 const contextDefaultValues: TodosContextState = {
   todos: [],
+  editTodoId: "",
   addTodo: () => {},
   removeTodo: () => {},
   markCompleted: () => {},
-}
+  editTodo: () => {},
+  updateTodoFunc: () => {},
+};
 
 export const TodosContext = createContext<TodosContextState>(
-  contextDefaultValues,
-)
+  contextDefaultValues
+);
 interface IProp {
-  children: ReactNode
+  children: ReactNode;
 }
 
 const TodosProvider: FC<IProp> = ({ children }) => {
-  const [todos, setTodos] = useState<Itodos[]>(contextDefaultValues?.todos)
+  const [todos, setTodos] = useState<Itodos[]>(contextDefaultValues?.todos);
+  const [editTodoId, setEditTodoId] = useState<string>("");
   const addTodo = (title: string) => {
     setTodos((todos: Itodos[]) => [
       ...todos,
       { id: Math?.random()?.toString(), title, completed: false },
-    ])
-  }
+    ]);
+  };
+  const updateTodoFunc = (id, title, completed) => {
+    const found = todos?.filter((el) => el?.id !== id);
+    console.log("id, title, completed :", id, title, completed);
+    let updatedTodos = [...found, { id, title, completed }];
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    setTodos(updatedTodos);
+  };
   const removeTodo = (id: string) => {
-    const updatedTodos = todos?.filter((el) => el?.id !== id)
-    setTodos(updatedTodos)
-  }
+    console.log("Want delete : ", id);
+    const updatedTodos = todos?.filter((el) => el?.id !== id);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+    setTodos(updatedTodos);
+  };
   const markCompleted = (id: string) => {
     const updatedTodo = todos?.map((el) =>
-      el?.id === id ? { ...el, completed: !el?.completed } : el,
-    )
-    setTodos(updatedTodo)
-  }
+      el?.id === id ? { ...el, completed: !el?.completed } : el
+    );
+    setTodos(updatedTodo);
+  };
+  const editTodo = (id: string) => {
+    setEditTodoId(id);
+  };
 
-  useEffect(() =>
-  {
+  useEffect(() => {
     // const todosLocal = JSON.parse(localStorage.getItem('todos') || '{}')
     if (todos?.length) {
-      localStorage.setItem('todos', JSON.stringify(todos))
-    } 
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
     // else {
     //   console.log('remove')
     //   localStorage.removeItem('todos')
     // }
-  }, [todos])
+  }, [todos]);
   useEffect(() => {
-    const todosLocal = JSON.parse(localStorage.getItem('todos') || '{}')
+    const todosLocal = JSON.parse(localStorage.getItem("todos") || "{}");
 
     if (!todos?.length && todosLocal?.length) {
-      setTodos(todosLocal)
+      setTodos(todosLocal);
     }
-  }, [todos])
+  }, [todos]);
 
   return (
     <TodosContext.Provider
       value={{
         todos,
+        editTodoId,
         addTodo,
         removeTodo,
         markCompleted,
+        editTodo,
+        updateTodoFunc,
       }}
     >
       {children}
     </TodosContext.Provider>
-  )
-}
+  );
+};
 
-export default TodosProvider
+export default TodosProvider;

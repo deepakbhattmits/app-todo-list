a37264247615e4264671a8a789a41994a0044e77
deepakbhattmits <deepakbhattmits@gmail.com>
2023-07-25 13:21:03 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, FC, Fragment, useContext } from 'react'
+import { useState, FC, Fragment, useContext, useEffect } from "react";
 import {
   Form,
   Input,
@@ -8,66 +8,133 @@ import {
   List,
   Typography,
   Divider,
-} from 'antd'
-import styles from './App.module.less'
-import { CloseCircleTwoTone, CheckCircleTwoTone } from '@ant-design/icons'
-import TodosProvider, { TodosContext } from './TodosContext'
+  Modal,
+} from "antd";
+import styles from "./App.module.less";
+import { CloseCircleTwoTone, CheckCircleTwoTone } from "@ant-design/icons";
+import TodosProvider, { TodosContext } from "./TodosContext";
 
-import './App.less'
-import ButtonComp from './components/ButtonComp'
+import "./App.less";
+import ButtonComp from "./components/ButtonComp";
 // import InputComp from './components/InputComp'
-import Layout from './components/Layout'
-import Counts from './components/Counts';
+import Layout from "./components/Layout";
+import Counts from "./components/Counts";
+import { Itodos } from "./TodosContext/types";
 const Todos: FC = (): JSX.Element => {
-  const [form] = Form.useForm()
-  const [todo, setTodo] = useState<string>('')
-  const { todos, addTodo, removeTodo, markCompleted } = useContext(TodosContext)
+  const [form] = Form.useForm();
+  const [todo, setTodo] = useState<string>("");
+  const [editTodoInput, setEditTodoInput] = useState<string>("");
+  const {
+    todos,
+    editTodoId,
+    addTodo,
+    removeTodo,
+    markCompleted,
+    editTodo,
+    updateTodoFunc,
+  } = useContext(TodosContext);
   const handleSubmit = () => {
-    addTodo(todo)
-    setTodo('')
+    addTodo(todo);
+    setTodo("");
     form.setFieldsValue({
-      Todo: '',
-    })
-    message.success('Submit success!')
-  }
+      Todo: "",
+    });
+    message.success("Submit success!");
+  };
   const onFinishFailed = () => {
-    message.error('Submit failed!')
-  }
+    message.error("Submit failed!");
+  };
   const onFill = () => {
     form.setFieldsValue({
-      Todo: 'Demo Todo',
-    })
-    setTodo('Demo Todo')
-  }
+      Todo: "Demo Todo",
+    });
+    setTodo("Demo Todo");
+  };
+  const handleEdit = () => {
+    let foundTodo: Itodos | undefined = todos?.find(
+      (todo) => !!todo.id?.match(editTodoId)?.length
+    );
+    let updateTodo = { ...foundTodo, title: editTodoInput };
+
+    console.log("updateTodo : ", updateTodo);
+    if (!!Object.keys(updateTodo)?.length) {
+      updateTodoFunc(updateTodo?.id, updateTodo?.title, updateTodo?.completed);
+    }
+    editTodo("");
+    message.success("Update of todo is success!");
+  };
 
   const Header: FC = (): JSX.Element => (
     <div className={`${styles.main} table__header--wrapper`}>
-      
       {todos?.length > 0 ? (
         <>
           <div>
-          <span>Total Todo{todos?.length>1? 's':''}: </span>
-          <span>
-            {todos?.length} todo{todos?.length > 1 ? 's ' : ' '}
-          </span>
+            <span>Total Todo{todos?.length > 1 ? "s" : ""}: </span>
+            <span>
+              {todos?.length} todo{todos?.length > 1 ? "s " : " "}
+            </span>
           </div>
           <div>
-
-        <span>
-            {todos?.filter(({completed}) => completed)?.length>0? `${todos?.filter(({completed}) => completed)?.length} Completed `:null}
+            <span>
+              {todos?.filter(({ completed }) => completed)?.length > 0
+                ? `${
+                    todos?.filter(({ completed }) => completed)?.length
+                  } Completed `
+                : null}
             </span>
             <span>
-            {todos?.filter(({completed}) => !completed)?.length>0 &&todos?.filter(({completed}) => completed)?.length>0? ', ' : ' '}
-          </span>
-          <span>
-            {todos?.filter(({completed}) => !completed)?.length>0? `${todos?.filter(({completed}) => !completed)?.length} Incompleted`:null}
-          </span>
+              {todos?.filter(({ completed }) => !completed)?.length > 0 &&
+              todos?.filter(({ completed }) => completed)?.length > 0
+                ? ", "
+                : " "}
+            </span>
+            <span>
+              {todos?.filter(({ completed }) => !completed)?.length > 0
+                ? `${
+                    todos?.filter(({ completed }) => !completed)?.length
+                  } Incompleted`
+                : null}
+            </span>
           </div>
         </>
       ) : null}
     </div>
-  )
-
+  );
+  // const RenderContent = (
+  //   editTodoInput,
+  //   setEditTodoInput: { editTodoInput: string; setEditTodoInput: ()=>void }
+  // ) => {
+  //   useEffect(() => {
+  //     const foundEditTodo: Itodos | undefined = todos?.find(
+  //       (todo) => !!todo.id?.match(editTodoId)?.length
+  //     );
+  //     if (!!foundEditTodo?.title) {
+  //       setEditTodoInput(!!foundEditTodo?.title ? foundEditTodo?.title : "");
+  //     }
+  //   },);
+  //   return (
+  //     <>
+  //       <Input
+  //         data-testid="todo-input"
+  //         placeholder="Todo"
+  //         value={editTodoInput}
+  //         allowClear
+  //         onChange={(e) => {
+  //           console.log("UPDATE :", e);
+  //           setEditTodoInput(e?.target?.value);
+  //         }}
+  //       />
+  //     </>
+  //   );
+  // };
+  useEffect(() => {
+    const foundEditTodo: Itodos | undefined = todos?.find(
+      (todo) => !!todo.id?.match(editTodoId)?.length
+    );
+    if (!!foundEditTodo?.title) {
+      setEditTodoInput(!!foundEditTodo?.title ? foundEditTodo?.title : "");
+    }
+  }, [editTodoId, todos]);
   return (
     <div className="TodoArea">
       <div>
@@ -79,29 +146,37 @@ const Todos: FC = (): JSX.Element => {
           onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
-          <div style={{ overflow: 'hidden' }}>
+          <div style={{ overflow: "hidden" }}>
             <Form.Item
               name="Todo"
               label="Enter todo"
-              rules={[{ required: true }, { type: 'string', min: 6 }]}
+              rules={[{ required: true }, { type: "string", min: 6 }]}
             >
               <Input
-                data-testid='todo-input'
+                data-testid="todo-input"
                 placeholder="Todo"
                 value={todo}
                 allowClear
                 onChange={(e) => {
-                  setTodo(e?.target?.value)
+                  setTodo(e?.target?.value);
                 }}
               />
             </Form.Item>
           </div>
           <Form.Item>
             <Space>
-              <Button data-testid='todo-submit' type="primary" htmlType="submit">
+              <Button
+                data-testid="todo-submit"
+                type="primary"
+                htmlType="submit"
+              >
                 Submit
               </Button>
-              <Button data-testid='todo-fill' htmlType="button" onClick={onFill}>
+              <Button
+                data-testid="todo-fill"
+                htmlType="button"
+                onClick={onFill}
+              >
                 Fill
               </Button>
             </Space>
@@ -113,13 +188,13 @@ const Todos: FC = (): JSX.Element => {
           <Fragment>
             <Divider orientation="left">Todo List</Divider>
             <List
-              data-testid='todo-list'
+              data-testid="todo-list"
               header={<Header />}
               // footer={<div>Footer</div>}
               bordered
               dataSource={todos}
               renderItem={({ id, title, completed }) => (
-                <List.Item data-testid='todo-item'>
+                <List.Item data-testid="todo-item">
                   <div className="left">
                     {completed ? (
                       <CheckCircleTwoTone twoToneColor="green" />
@@ -127,7 +202,7 @@ const Todos: FC = (): JSX.Element => {
                       <CloseCircleTwoTone twoToneColor="#eb2f96" />
                     )}
                     <Typography.Text
-                      style={{ marginLeft: '10px' }}
+                      style={{ marginLeft: "10px" }}
                       delete={completed}
                     >
                       {title}
@@ -135,9 +210,15 @@ const Todos: FC = (): JSX.Element => {
                   </div>
                   <div className="right">
                     <ButtonComp
-                      style={{ marginRight: '10px' }}
-                      btnText={completed ? 'Completed' : 'Incomplete'}
-                      type={completed ? 'default' : 'primary'}
+                      style={{ marginRight: "10px" }}
+                      btnText="Edit"
+                      type="primary"
+                      onClick={() => editTodo(id)}
+                    />
+                    <ButtonComp
+                      style={{ marginRight: "10px" }}
+                      btnText={completed ? "Completed" : "Incomplete"}
+                      type={completed ? "default" : "primary"}
                       onClick={() => markCompleted(id)}
                     />
                     <ButtonComp
@@ -153,14 +234,36 @@ const Todos: FC = (): JSX.Element => {
           {/* ))} */}
         </ul>
       </div>
+      <Modal
+        title="Vertically centered modal dialog"
+        centered
+        open={!!editTodoId?.length}
+        onOk={handleEdit}
+        onCancel={() => editTodo("")}
+      >
+        {/* <RenderContent
+          editTodoInput={editTodoInput}
+          setEditTodoInput={setEditTodoInput}
+        /> */}
+        <Input
+          data-testid="todo-input"
+          placeholder="Todo"
+          value={editTodoInput}
+          allowClear
+          onChange={(e) => {
+            console.log("UPDATE :", e);
+            setEditTodoInput(e?.target?.value);
+          }}
+        />
+      </Modal>
     </div>
-  )
-}
+  );
+};
 const App: FC = (): JSX.Element => (
   <TodosProvider>
     <Layout headerContent="My todo list" />
     <Todos />
-    <Counts/>
+    <Counts />
   </TodosProvider>
-)
-export default App
+);
+export default App;
